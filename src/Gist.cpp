1d65ede498e87a3f2d259d13266a92dd72436f7d
Joseph Diza <josephm.diza@gmail.com>
2021-06-19 15:22:10 -0700

    Create gist request with httplib

--- src/Gist.cpp
+++ src/Gist.cpp
@@ -1,41 +1,38 @@
+//#define CPPHTTPLIB_OPENSSL_SUPPORT
 #include <iostream>
+#include <cstdlib> /* getenv */
 
 #include <string>
-#include <cpr/cpr.h>
 #include <fmt/core.h>
 
+//#include <cpr/cpr.h>
+#include "httplib.h"
+
 int main(int argc, char** argv) { 
-  cpr::Response r = cpr::Get(
-      //{"https://api.github.com/gists"},
-      //{"https://api.github.com/gists"},
-      //cpr::Header{{"accept", "application/vnd.github.v3+json"}}
-      //cpr::Header{{"Accept", "application/vnd.github.v3+json"}}
-      //cpr::Url{"http://www.httpbin.org/headers"}, 
-      //cpr::Url{"https://api.github.com/gists"},
-      //cpr::Url{"https://gist.github.com/jmdaemon/gists"},
-      //cpr::Header{{"accept", "application/vnd.github.v3+json"}}
-      //cpr::Header{{"accept", "application/json"}}
-      //cpr::Url{"https://api.getgist.com/contacts"},
-      //cpr::Parameters{{"id", "josephm.diza@gmail.com"}}
-      //cpr::Url{"http://www.httpbin.org/get"}
-      //cpr::Url{"https://api.github.com/gists"}
-      //cpr::Url{"https://api.github.com/users/octocat/orgs"},
-      cpr::Url{"https://api.github.com/"},
-      cpr::Header{
-      {"User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0"}, 
-      {"accept", "application/json"}},
-      //{"Accept", "application/vnd.github.v3+json"}},
-      cpr::Timeout{1000}
-      );
+  //cpr::Response r = cpr::Get(cpr::Url{"https://api.github.com/"});
 
-  //fmt::print("Request URL           : {}\n", r.url);
-  std::cout<<"Request URL           : " << r.url << std::endl;
-  fmt::print("Request Status Code   : {}\n", r.status_code);                  // 200 
-  fmt::print("Request Content Type  : {}\n", r.header["content-type"]);       // application/json; charset=utf-8 
-  fmt::print("Request Text          : {}\n", r.text);                         // JSON text string
+  ////fmt::print("Request URL           : {}\n", r.url);
+  //std::cout<<"Request URL           : " << r.url << std::endl;
+  //fmt::print("Request Status Code   : {}\n", r.status_code);                  // 200 
+  //fmt::print("Request Content Type  : {}\n", r.header["content-type"]);       // application/json; charset=utf-8 
+  //fmt::print("Request Text          : {}\n", r.text);                         // JSON text string
 
-  //fmt::print("Request Status Code: {}\n", r.status_code);
-  //fmt::print("Request Header: {}\n", r.raw_header);
-  //fmt::print("Request Text: {}\n", r.text);
+
+
+  const char* GIST_FILE_PATH = std::getenv("GIST_CONFIG_HOME");
+  if (GIST_FILE_PATH) {
+    //std::string GIST_CONFIG_HOME(std::getenv("GIST_CONFIG_HOME"));
+    std::string GIST_CONFIG_HOME(GIST_FILE_PATH);
+    fmt::print("$GIST_CONFIG_HOME: {}\n", GIST_CONFIG_HOME);
+  }
+
+  // http-lib
+  httplib::Client cli("https://api.github.com");
+  httplib::Headers headers = { 
+    { "Accept", "application/vnd.github.v3+json"}
+  };
+  auto res = cli.Get("/gists", headers);
+  fmt::print("Response Status : {}\n", res->status);
+  fmt::print("Response Body   : {}\n", res->body);
   return 0;
 }
