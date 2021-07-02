93b9cf05077aa788aa9dcd8fddc5e3e1b6cef218
Joseph Diza <josephm.diza@gmail.com>
2021-07-02 12:56:57 -0700

    Create and get user gists

--- gists.py
+++ gists.py
@@ -4,30 +4,25 @@ import json
 import toml
 from pathlib import Path
 
-def create_header():
-    header = { 'Accept': 'application/vnd.github.v3+json' }
-    return header
-
 # Print
-def print_response(r):
-    print(f"Request Status Code     {r.status_code}")
-    print(f"Request Content Type    {r.headers['content-type']}")
-    print(f"Request Encoding        {r.encoding}")
-    print(f"Request Body            {r.text}")
-    print(f"Request Json            {r.json()}")
-    print(f"Request Content         {r.content}")
+def print_response(res):
+    print(f"Request Status Code     {res.status_code}")
+    print(f"Request Content Type    {res.headers['content-type']}")
+    print(f"Request Encoding        {res.encoding}")
+    print(f"Request Body            {res.text}")
+    print(f"Request Json            {res.json()}")
+    print(f"Request Content         {res.content}")
 
-def list_packages(r, pkg_list):
+def list_packages(res, pkg_list):
     print(f"Package List: {pkg_list}")
-    print(r.text)
+    print(res.text)
 
-def pretty_print(r, json_file=False):
-# def pretty_print(gist, json_file=False):
-    if (json_file):
-        parsed_response = json.loads(r.text)
+def pretty_print(res, display_json=False):
+    if (display_json):
+        parsed_response = json.loads(res.text)
         print(json.dumps(parsed_response, indent=4, sort_keys=True))
     else:
-        print_response(r)
+        print_response(res)
 
 # File
 def read_file(filename):
@@ -41,24 +36,25 @@ def read_file(filename):
         file.close()
 
 def create_json(local_name, gist_name, desc, contents, public=False):
-# def create_json(gist_name, desc, contents, public=False):
     data = { 'description': desc, 'public': public }
     data['files'] = {}
     data['files'][gist_name] = { 'content': contents }
     result = json.dumps(data)
     print(result)
     return result
-    # with open(local_name, 'w') as output:
     with open(local_name, 'w') as output:
         json.dump(data, output)
 
 # Request
+def create_header():
+    header = { 'Accept': 'application/vnd.github.v3+json' }
+    return header
+
 def send_get(url, token):
     r   = requests.get(url, allow_redirects=True, headers=create_header(),
                         auth=('Authentication', token))
     return r
 
-# def send_post(url, token, filename):
 def send_post(url, token, filename):
     contents = open(filename, 'rb').read()
     r   = requests.post(url, allow_redirects=True, headers=create_header(),
@@ -68,51 +64,48 @@ def send_post(url, token, filename):
 
 # Gist
 class Gist():
-    def __init__(self, url, config, gist_name, desc):
-        self.url = url
-        self.config = config
-        self.gist_name = gist_name
-        self.desc = desc
+    def __init__(self, url, config, gist_name, desc, public):
+        self.url        = url
+        self.token      = read_file(config)
+        self.config     = config
+        self.gist_name  = gist_name
+        self.desc       = desc
+        self.public     = public
+
+    def list_gists(self, display_json):
+        pretty_print(get_gists(self), display_json)
 
     def create_gist(self, local_name, contents):
-        # create_json(gist_name, desc, contents) 
-        create_json(local_name, gist_name, desc, contents, public=False)
-        token = read_file(config)
-        return (send_post(url, token, local_name))
+        create_json(local_name, self.gist_name, self.desc, contents, self.public)
+        res = send_post(self.url, self.token, local_name)
+        return res
 
     def get_gists(self):
-        token = read_file(config)
-        # aur_list_pkgs, arch_list_pkgs = pretty_print(send_get(url, token), json_file=True)
-        # return aur_list_pkgs, arch_list_pkgs = pretty_print(send_get(url, token), json_file=True) 
-        r = send_get(url, token)
-        parsed_response = json.loads(r.text)
-        aur_list_pkgs   = parsed_response[0]['files']['aur-list.pkg']['raw_url']
-        arch_list_pkgs  = parsed_response[1]['files']['pacman-list.pkg']['raw_url']
-        print("Aur      Raw Url: {aur_list_pkgs} ")
-        print("Pacman   Raw Url: {arch_list_pkgs}")
-        # list_packages((send_get(aur_list_pkgs, token)), "Aur")
-        # list_packages((send_get(arch_list_pkgs, token)), "Arch")
+        res = send_get(self.url, self.token)
+        return res
+
 
 url = "https://api.github.com/gists"
 config = ".config/gist/config.toml"
 
-# token = read_file(config)
-# print_response(send_post(token))
-# send_post(token)
-# create_json("TestFile.txt", "TestFile2.json", "Creating a test file via GitHub Gist API")
-# 'TestFile.json'
+# == Create User Gists ==
+# local_name  = "TestFile2.json"
+# gist_name   = "TestFile2"
+# desc        = "Creating a test file via GitHub Gist API"
+# contents    = "This is a test file."
+# public      = False
+# aur_gist    = Gist(url, config, gist_name, desc, public)
 
-local_name  = "TestFile2.json"
-gist_name   = "TestFile2"
-desc        = "Creating a test file via GitHub Gist API"
-contents    = "This is a test file."
-aur_gist    = Gist(url, config, gist_name, desc)
-pretty_print(aur_gist.create_gist(local_name, contents), json_file=False)
+# res = aur_gist.create_gist(local_name, contents)
+# pretty_print(res, display_json=True)
+# print(f"Gist: {gist_name} Created!")
 
-# get_gists(url, config)
+# == Get User Gists ==
 
-
-
-# print(f"Saving as: {directory}/{names.get()}.zip", flush=True)
-# if (mini.status_code != 404):
-    # writeToFile(mini.content, f"{directory}/{names.get()}.zip", 'wb')
+json_response   = send_get(url, read_file(config)).json()
+aur_list_pkgs   = json_response[0]['files']['aur-list.pkg']['raw_url']
+arch_list_pkgs  = json_response[1]['files']['pacman-list.pkg']['raw_url']
+print(f"Aur    Raw: {aur_list_pkgs} ")
+print(f"Pacman Raw: {arch_list_pkgs}")
+# list_packages((send_get(aur_list_pkgs, token)), "Aur")
+# list_packages((send_get(arch_list_pkgs, token)), "Arch")
