6c2f238539c2347d83e21c18bc5671afe732e344
Joseph Diza <josephm.diza@gmail.com>
2021-07-03 17:59:34 -0700

    Update a gist

--- gists.py
+++ gists.py
@@ -42,8 +42,8 @@ def create_json(local_name, gist_name, desc, contents, public=False):
     result = json.dumps(data)
     print(result)
     return result
-    with open(local_name, 'w') as output:
-        json.dump(data, output)
+    # with open(local_name, 'w') as output:
+        # json.dump(data, output)
 
 # Request
 def create_header():
@@ -58,6 +58,16 @@ def send_get(url, token):
 def send_post(url, token, filename):
     contents = open(filename, 'rb').read()
     r   = requests.post(url, allow_redirects=True, headers=create_header(),
+                        auth=('Authentication', token),
+                        data=contents)
+    return r
+
+# def send_patch(url, token, filename):
+    # contents = open(filename, 'rb').read()
+# def send_patch(url, token, filename):
+def send_patch(url, token, contents):
+    # contents = open(filename, 'rb').read()
+    r   = requests.patch(url, allow_redirects=True, headers=create_header(),
                         auth=('Authentication', token),
                         data=contents)
     return r
@@ -84,6 +94,12 @@ class Gist():
         res = send_get(self.url, self.token)
         return res
 
+    # def update_gist(self, url=self.url, config=self.config, local_name=self.local_name,
+                    # gist_name=self.gist_name, desc=self.desc, public=self.public, contents):
+        # # json_file = create_json(local_name, gist_name, desc, contents, public) 
+        # create_json(local_name, gist_name, desc, contents, public)
+        # res = send_patch(
+
 url = "https://api.github.com/gists"
 config = ".config/gist/config.toml"
 
@@ -100,29 +116,55 @@ config = ".config/gist/config.toml"
 # pretty_print(res, display_json=True)
 
 # == Get User Gists ==
-res             = send_get(url, read_file(config))
-json_res        = json.loads(res.text)
+# res             = send_get(url, read_file(config))
+# json_res        = json.loads(res.text)
 gist_names      = [ 'aur-list.pkg', 'pacman-list.pkg' ]
-print(f"Retrieved User Gists")
+# print(f"Retrieved User Gists")
 # pretty_print(res, display_json=True)
 
-raw_urls = []
+# raw_urls = []
+# for file in range(0, len(json_res)):
+    # key = (next(iter(json_res[file]['files'])))
+    # for filename in range(0, len(gist_names)):
+        # if key == gist_names[filename]:
+            # raw_urls.append(json_res[file]['files'][key]['raw_url'])
+
+# print("\nRaw Urls:")
+# for url in raw_urls:
+    # print(url)
+
+# gist_dict_keys = ["AUR", "Arch"]
+
+# raw_urls_dict = {}
+# for url in range(0, len(raw_urls)):
+    # raw_urls_dict[gist_dict_keys[url]] = raw_urls[url]
+
+# print("\nRaw Urls Dict:")
+# for key, url in raw_urls_dict.items():
+    # print(f'Key: {key}\t\t\tValue: {url}')
+
+# == Update Gist ==
+res             = send_get(url, read_file(config))
+json_res        = json.loads(res.text)
+
+ids = []
 for file in range(0, len(json_res)):
+    # idx = (next(iter(json_res[file]['id'])))
     key = (next(iter(json_res[file]['files'])))
     for filename in range(0, len(gist_names)):
-        if key == gist_names[filename]:
-            raw_urls.append(json_res[file]['files'][key]['raw_url'])
+        if key != gist_names[filename]:
+            idx = json_res[file]['id']
+            ids.append(json_res[file]['id'])
+            print(idx)
 
-print("\nRaw Urls:")
-for url in raw_urls:
-    print(url)
+local_name  = "TestFile3.json"
+gist_name   = "TestFile3"
+desc        = "Updating a test file via GitHub Gist API"
+contents    = "This is an updated test file."
+public      = False
 
-gist_dict_keys = ["AUR", "Arch"]
-
-raw_urls_dict = {}
-for url in range(0, len(raw_urls)):
-    raw_urls_dict[gist_dict_keys[url]] = raw_urls[url]
-
-print("\nRaw Urls Dict:")
-for key, url in raw_urls_dict.items():
-    print(f'Key: {key}\t\t\tValue: {url}')
+url = f'{url}/{ids[0]}'
+print(url)
+token = read_file(config)
+json_file = create_json(local_name, gist_name, desc, contents, public)
+print_response(send_patch(url, token, json_file))
