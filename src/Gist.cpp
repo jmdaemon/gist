bee3a29370ce63fd61995f8c35a92ce9296c1754
Joseph Diza <josephm.diza@gmail.com>
2021-07-13 23:53:09 -0700

    Change createJson params, implement showUsage

--- src/Gist.cpp
+++ src/Gist.cpp
@@ -12,24 +12,23 @@
 using nlohmann::json;
 
 void showUsage() {
-  fmt::print("Show Program Usage\n");
+  fmt::print("Usage: gist [id/filename] [desc] [contents]");
+  fmt::print("  -h, --help      Show this message and exit\n");
+  fmt::print("  -c,             Create a new gist\n");
+  fmt::print("  -u id,          Update an existing gist specified by id\n");
+  fmt::print("  -u fname,       Update an existing gist specified by filename\n\n");
 }
 
 void printResponse(RestClient::Response res) {
-  fmt::print("Response Code     : {}\n", res.code);
-  fmt::print("Response Body     : {}\n", res.body);
-}
-
-void prettyPrint(RestClient::Response res) {
   std::string response = json::parse(res.body).dump(4);
   fmt::print("{}\n", response);
 }
 
-std::string createJson(std::string desc, std::string filename, std::string content, bool pub = false) {
+std::string createJson(Data data) {
   json o;
-  o["description"]                = desc;
-  o["public"]                     = pub;
-  o["files"][filename]["content"] = content;
+  o["description"]                  = data.desc;
+  o["public"]                       = data.pub;
+  o["files"][data.fname]["content"] = data.contents;
   return o.dump();
 }
 
@@ -60,10 +59,10 @@ json listGists(std::string url, std::string username, std::optional<std::string>
 void updateGist(Data data, std::string url, std::optional<std::string> token, std::string msg) {
   fmt::print("{}\n", msg);
   printData(data);
-  auto contents  = createJson(data.desc, data.fname, data.contents); 
-  auto conn = createConnection(url, token);
-  auto res  = sendPATCH(conn, data.id, contents);
-  prettyPrint(res);
+  auto contents = createJson(data);
+  auto conn     = createConnection(url, token);
+  auto res      = sendPATCH(conn, data.id, contents);
+  printResponse(res);
 }
 
 int main(int argc, char** argv) {
@@ -89,30 +88,29 @@ int main(int argc, char** argv) {
   fmt::print("Username : [{}]\n\n", *username);
 
   RestClient::init();
-
   std::string id    = (input.argExists("id"))     ? input.getArg("id")      : "";
   std::string fname = (input.argExists("fname"))  ? input.getArg("fname")   : "";
-  std::string contents = "";
 
   if (input.argExists("-l")) {
     // List user gists
+
     fmt::print("Listing all gists for {}\n", *username);
     listGists(url, *username, token);
   } else if (input.argExists("-c")) { 
+
     // Create new gist for user
     fmt::print("Creating gist for {}\n", *username);
     auto conn = createConnection(url, token);
     Data data = Data{id, readInput(), readInput(), readInput()};
     printData(data);
 
-    contents = createJson(data.desc, data.fname, data.contents);
+    std::string contents = createJson(data);
     auto res = createGist(conn, contents);
-    prettyPrint(res);
+    printResponse(res);
 
   } else if (input.argExists("-u")) { 
     // Update existing gist for user
     auto o = listGists(url, *username, token);
-
     Data data;
     if (!id.empty())
       data = {id, getFilename(o, id), readInput(), readInput()};
@@ -121,7 +119,6 @@ int main(int argc, char** argv) {
 
     updateGist(data, url, token, fmt::format(fmt::runtime("Updating gist {} for {}\n"), data.id, *username));
   }
-
   RestClient::disable();
   return 0; 
 }
