9ed7fcdacbcaa9e5d240044bd9a21fc4e0e9c08c
Joseph Diza <josephm.diza@gmail.com>
2021-09-22 20:35:25 -0700

    Fix .deb, .tgz, .zip package creation

--- configure.sh
+++ configure.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-CC=clang CXX=clang++"" cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Debug
+#CC=clang CXX=clang++"" cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Debug
+CC=clang CXX=clang++"" cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Release
