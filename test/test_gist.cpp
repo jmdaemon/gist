516e3c6bc182e96f0b0e339f2cba21a28499d920
Joseph Diza <josephm.diza@gmail.com>
2022-08-24 02:12:17 -0700

    Add initial search_date test function

--- test/test_gist.cpp
+++ test/test_gist.cpp
@@ -2,6 +2,20 @@
 #include "doctest.h"
 #include "gist.h"
 
+#include <fmt/core.h>
+#include <file.h>
+
+// Constants
+static const std::string gist_file = "./samples/gists.p1.json";
+
+// Helper Functions
+void test_date(nlohmann::json json_res, std::tm tm, std::string date_type, RELTIME reltime) {
+    std::vector<nlohmann::json> results;
+    for (auto gist: json_res)
+      results.push_back(search_date(gist, tm, date_type, reltime));
+    show_results(results, false);
+}
+
 // Sanity checks
 TEST_CASE("JSON objects can be properly formatted") { 
   SUBCASE("C++ strings are formatted properly") {
@@ -14,22 +28,33 @@ TEST_CASE("JSON objects can be properly formatted") {
   }
   SUBCASE("C strings are formatted properly") {
     arguments args;
-    //const char* fname = "test.h";
-    //const char* conts = "int x = 4;";
-    //const char* desc  = "Test file with declared variable";
-    //bool priv  = false;
     args.gist.filename  = (char*) "test.h";
     args.gist.conts     = (char*) "int x = 4;";
     args.gist.desc      = (char*) "Test file with declared variable";
     args.priv           = false;
 
     auto res = create_json(args);
-    //nlohmann::json req;
-    //req["description"] = desc;
-    //req["public"] = !priv;
-    //req["files"][fname]["content"] = conts;
-
     CHECK(res["files"]["test.h"]["content"] == "int x = 4;");
   }
+}
 
+TEST_CASE("search_date() filters the correct dates") {
+  std::string gists(read_file(gist_file.c_str()));
+  auto date = "";
+  auto json_res = nlohmann::json::parse(gists);
+  auto date_type = "created_at";
+  std::tm tm = parse_datetime(date, GIST_DATE_FORMAT);
+
+  SUBCASE("Filter exact matches only") {
+    fmt::print("Exact matches\n");
+    test_date(json_res, tm, date_type, EXACT);
+  }
+  SUBCASE("Filter only later dates") {
+    fmt::print("Matches after date\n");
+    test_date(json_res, tm, date_type, AFTER);
+  }
+  SUBCASE("Filter only earlier dates") {
+    fmt::print("Matches before date\n");
+    test_date(json_res, tm, date_type, BEFORE);
+  }
 }
